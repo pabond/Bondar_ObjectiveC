34fd1dd15d9b83b50be70f796bb9dab878b104d7
Bondar Pavel <bond.p85@gmail.com>
2016-06-06 16:46:47 +0300

    ObjectiveCBond -> initial commit

---
+++ ObjectiveCBond/ObjectiveCBond/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveCBond
+//
+//  Created by Bondar Pavel on 6/6/16.
+//  Copyright © 2016 Pavel Bondar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+       
+    }
+    return 0;
+}
