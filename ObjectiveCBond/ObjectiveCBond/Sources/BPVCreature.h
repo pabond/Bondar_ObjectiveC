f1cfb2e84aaaefa5ebbae173538721a819ae336c
Bondar Pavel <bond.p85@gmail.com>
2016-06-07 18:37:50 +0300

    BPVCreature -> add interface

--- ObjectiveCBond/ObjectiveCBond/Sources/BPVCreature.h
+++ ObjectiveCBond/ObjectiveCBond/Sources/BPVCreature.h
@@ -16,6 +16,17 @@ typedef enum {
 
 @interface BPVCreature : NSObject
 
-@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy)     NSString  *name;
+@property (nonatomic, readonly) float     waight;
+@property (nonatomic, readonly) NSInteger age;
+@property (nonatomic, retain)   NSArray   *children;
+
+- (void)fight;
+- (BPVCreature *)giveBirthToChild;
+
+- (void)addChild: (BPVCreature *)child;
+- (void)removeChild: (BPVCreature *)child;
+
+- (void)sayHi;
 
 @end
